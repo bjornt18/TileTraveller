472d8a458278741d0b2d872a80dea4c81904167f
Björn Breki <bjornt18@ru.is>
2018-09-18 13:27:00 +0000

    algorithm

---
+++ TileTraveller1.py
@@ -0,0 +1,3 @@
+#Byrja á því að búa til svæðið sem leikmaðurinn er í, leyfa leikmanninum að færa sig í átt sem hægt er að færa sig í. Setja síðan inn markmiðið, staðinn sem leikmaðurinn þarf að komast til að vinna
+
+
