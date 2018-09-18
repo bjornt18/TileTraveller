19e66e0d69df7f460ca879d0cd5e08357b9bb8cd
Björn Breki <bjornt18@ru.is>
2018-09-18 15:26:41 +0000

    working

--- TileTraveller1.py
+++ TileTraveller1.py
@@ -1,32 +1,41 @@
 #Byrja á því að búa til svæðið sem leikmaðurinn er í, leyfa leikmanninum að færa sig í átt sem hægt er að færa sig í. Setja síðan inn markmiðið, staðinn sem leikmaðurinn þarf að komast til að vinna
 position = 1.1
 att = "(N)orth"
+villa = 0
 while position != 3.1:
-    print("You can travel:", att, position)
-    direction = input("Direction: ")
-    direction = direction.lower()
+    if villa == 1:
+        direction = input("Direction: ").lower()
+        villa = villa - 1
+    else:
+        print("You can travel:", att, position)
+        direction = input("Direction: ").lower()
     if direction == "n":
         if position == 1.1 or position == 1.2 or position == 3.2 or position == 2.1:
             position = position + 0.1
         else:
             print("Not a valid direction!")
+            villa = villa + 1
     elif direction == "s":
         if position == 1.2 or position == 1.3 or position == 2.2 or position == 3.3 or position == 3.2:
             position = position - 0.1
         else:
             print("Not a valid direction!")
+            direction = input("Direction: ").lower()
     elif direction == "w":
         if position == 2.3 or position == 2.2 or position == 3.3:
             position = position - 1
         else:
             print("Not a valid direction!")
+            direction = input("Direction: ").lower()
     elif direction == "e":
         if position == 1.2 or position == 1.3 or position == 2.3:
             position = position + 1
         else:
             print("Not a valid direction!")
+            direction = input("Direction: ").lower()
     else:
         print("Not a valid direction!")
+        direction = input("Direction: ").lower()
     position = round(position, 1)
     if position == 1.1:
         att = "(N)orth"
