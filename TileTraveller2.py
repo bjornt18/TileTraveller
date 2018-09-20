3ff1207609edec31fd30c1458a04ebdbb24c0241
Björn Breki <bjornt18@ru.is>
2018-09-20 12:35:16 +0000

    def att

--- TileTraveller2.py
+++ TileTraveller2.py
@@ -1,13 +1,33 @@
 #Byrja á því að búa til svæðið sem leikmaðurinn er í, leyfa leikmanninum að færa sig í átt sem hægt er að færa sig í. Setja síðan inn markmiðið, staðinn sem leikmaðurinn þarf að komast til að vinna
+def att(a):
+    if a == 1.1:
+        att = "(N)orth."
+    if a == 1.2:
+        att = "(N)orth or (E)ast or (S)outh."
+    if a == 1.3:
+        att = "(E)ast or (S)outh."
+    if a == 2.1:
+        att = "(N)orth."
+    if a == 2.2:
+        att = "(S)outh or (W)est."
+    if a == 2.3:
+        att = "(E)ast or (W)est."
+    if a == 3.1:
+        att = "(N)orth."
+    if a == 3.2:
+        att = "(N)orth or (S)outh."
+    if a == 3.3:
+        att = "(S)outh or (W)est."
+    return att
+
 position = 1.1
-att = "(N)orth."
 villa = 0
 while position != 3.1:
     if villa == 1:
         direction = input("Direction: ").lower()
         villa = villa - 1
     else:
-        print("You can travel:", att)
+        print("You can travel:", att(position))
         direction = input("Direction: ").lower()
     if direction == "n":
         if position == 1.1 or position == 1.2 or position == 3.2 or position == 2.1:
@@ -37,23 +57,5 @@ while position != 3.1:
         print("Not a valid direction!")
         villa = villa + 1
     position = round(position, 1)
-    if position == 1.1:
-        att = "(N)orth."
-    if position == 1.2:
-        att = "(N)orth or (E)ast or (S)outh."
-    if position == 1.3:
-        att = "(E)ast or (S)outh."
-    if position == 2.1:
-        att = "(N)orth."
-    if position == 2.2:
-        att = "(S)outh or (W)est."
-    if position == 2.3:
-        att = "(E)ast or (W)est."
-    if position == 3.1:
-        att = "(N)orth."
-    if position == 3.2:
-        att = "(N)orth or (S)outh."
-    if position == 3.3:
-        att = "(S)outh or (W)est."
 print("Victory!")
 #Github: https://github.com/bjornt18/TileTraveller
